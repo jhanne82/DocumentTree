2af79b516ff5ccc115db533ba0498fa8101df178
jhanne82 <jasmin.hannemann@googlemail.com>
2017-04-04 15:36:23 +0200

    - extend gitignore - add test class - add JUnit

---
+++ src/test/java/com/github/jhanne82/documenttree/search/BreadthFirstSearchTest.java
@@ -0,0 +1,17 @@
+package com.github.jhanne82.documenttree.search;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+
+public class BreadthFirstSearchTest {
+
+
+    @Test
+    public void test() {
+        assertTrue( true );
+    }
+
+}
