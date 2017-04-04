e8948a5ddec7787f446f77e18d336d02f1fc6129
jhanne82 <jasmin.hannemann@googlemail.com>
2017-04-04 17:57:14 +0200

    - added Document object - added utils class to calculate eulerian distance and transform value into relevance value

---
+++ src/main/java/com/github/jhanne82/component/Document.java
@@ -0,0 +1,69 @@
+package com.github.jhanne82.component;
+
+
+import org.apache.commons.collections.buffer.CircularFifoBuffer;
+
+import java.math.BigDecimal;
+
+public class Document <T>{
+
+    private final CircularFifoBuffer relevanceBuffer;
+    private final T[] termVector;
+
+    private int   timestampOfLastSearch;
+
+
+
+    public Document( T[] termVector ){
+        this.relevanceBuffer = new CircularFifoBuffer( 20 );
+        this.termVector = termVector;
+    }
+
+
+    public Document( T[] termVector, int bufferSize ){
+        this.relevanceBuffer = new CircularFifoBuffer( bufferSize );
+        this.termVector = termVector;
+    }
+
+
+
+    public void setTimestampOfLastSearch( int timestampOfLastSearch ) {
+        this.timestampOfLastSearch = timestampOfLastSearch;
+    }
+
+
+    public int getTimestampOfLastSearch() {
+        return this.timestampOfLastSearch;
+    }
+
+
+    public T[] getTermVector() {
+        return this.termVector;
+    }
+
+    
+    public BigDecimal getAverageRelevance() {
+
+        BigDecimal averageRelevance = BigDecimal.ZERO;
+
+        for ( Object singleRelevance : relevanceBuffer ) {
+            averageRelevance.add( ( BigDecimal )singleRelevance );
+        }
+
+        averageRelevance = averageRelevance.divide( BigDecimal.valueOf( relevanceBuffer.size() ) );
+
+        return averageRelevance;
+    }
+
+
+
+    public void addRelevance( BigDecimal relevance ) {
+        relevanceBuffer.add( relevance );
+    }
+
+
+
+    public void clearRelevanceBuffer() {
+        relevanceBuffer.clear();
+    }
+}
---
+++ src/main/java/com/github/jhanne82/utils/EulerianDistance.java
@@ -0,0 +1,26 @@
+package com.github.jhanne82.utils;
+
+
+import java.math.BigDecimal;
+
+public class EulerianDistance {
+
+
+
+    public static <T>BigDecimal calEulerianDistance( T[] vectorA, T[] vectorB ) {
+
+        BigDecimal eulerianDistance = BigDecimal.ZERO;
+
+
+        return eulerianDistance;
+    }
+
+
+
+    public static BigDecimal transformEulerianDistanceToRelevanceValue( BigDecimal eulerianDistance ) {
+        double basis = 0.5;
+
+        return BigDecimal.valueOf( Math.pow( basis, eulerianDistance.doubleValue() ) );
+    }
+
+}
