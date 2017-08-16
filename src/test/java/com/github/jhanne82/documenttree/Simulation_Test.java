76dadd6d677ecaa370d5f3f98d34c26692aa4e7b
jhanne82 <jasmin.hannemann@googlemail.com>
2017-08-16 22:23:43 +0200

    - decrease parameter

--- src/test/java/com/github/jhanne82/documenttree/Simulation_Test.java
+++ src/test/java/com/github/jhanne82/documenttree/Simulation_Test.java
@@ -14,13 +14,19 @@ import java.util.List;
 public class Simulation_Test {
 
 
-    private static final int MAX_COUNT_OF_CREATED_DOCUMENTS = 1000;
+/*    private static final int MAX_COUNT_OF_CREATED_DOCUMENTS = 1000;
     private static final int MAX_COUNT_OF_CREATED_SEARCHES = 1000000;
     private static final int MAX_COUNT_OF_TERMS_USED_TO_DEFINE_VECTOR = 1000;
     private static final int MAX_COUNT_OF_TERMS_WITH_QUANTIFIER = 3;
     private static final int LIMIT_FOR_LOCAL_KNOWLEDGE = 300;
-    private static final int NUMBER_OF_SEARCHES_BEFORE_REPOSITIONING = 20;
+    private static final int NUMBER_OF_SEARCHES_BEFORE_REPOSITIONING = 20; */
 
+    private static final int MAX_COUNT_OF_CREATED_DOCUMENTS = 100;
+    private static final int MAX_COUNT_OF_CREATED_SEARCHES = 10000;
+    private static final int MAX_COUNT_OF_TERMS_USED_TO_DEFINE_VECTOR = 100;
+    private static final int MAX_COUNT_OF_TERMS_WITH_QUANTIFIER = 3;
+    private static final int LIMIT_FOR_LOCAL_KNOWLEDGE = 30;
+    private static final int NUMBER_OF_SEARCHES_BEFORE_REPOSITIONING = 5;
 
 
 
