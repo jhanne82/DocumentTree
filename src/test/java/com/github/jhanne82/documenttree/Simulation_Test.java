f725d81b9f193e4235c0327860f7dff3aab2751f
jhanne82 <jasmin.hannemann@googlemail.com>
2017-09-19 15:38:23 +0200

    - revert settings

--- src/test/java/com/github/jhanne82/documenttree/Simulation_Test.java
+++ src/test/java/com/github/jhanne82/documenttree/Simulation_Test.java
@@ -11,13 +11,10 @@ public class Simulation_Test {
     @Test
     public void doSimulation_1_1() {
 
-//        SimulationAppliction simulationAppliction = new SimulationAppliction( 100, 600,
-//                                                                              100, 3,
-//                                                                              50, 5);
+        SimulationAppliction simulationAppliction = new SimulationAppliction( 100, 600,
+                                                                              100, 3,
+                                                                              50, 5);
 
-        SimulationAppliction simulationAppliction = new SimulationAppliction( 1000, 1000000,
-                                                                              1000, 3,
-                                                                              300, 20);
         simulationAppliction.simulation_1_1();
     }
 
