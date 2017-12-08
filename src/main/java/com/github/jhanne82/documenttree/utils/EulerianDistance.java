dd8f8bf3584fa58dc6080b7e9b12a52debbec272
jhanne82 <jasmin.hannemann@googlemail.com>
2017-12-08 16:07:13 +0100

    - added documentation

--- src/main/java/com/github/jhanne82/documenttree/utils/EulerianDistance.java
+++ src/main/java/com/github/jhanne82/documenttree/utils/EulerianDistance.java
@@ -1,12 +1,17 @@
 package com.github.jhanne82.documenttree.utils;
 
 
-
+/**
+ * Implementation to compute a relevance between a document and a search vector. To calculate the
+ * relevance the eulerian distance will be used.
+ */
 public class EulerianDistance {
 
 
 
-    protected static double calEulerianDistance(Double[] documentVector, Double[] searchVector ) {
+    // calculate euler distance between document and search vector
+    // see https://en.wikipedia.org/wiki/Euclidean_distance#Definition
+    private static double calEulerianDistance(Double[] documentVector, Double[] searchVector ) {
 
         double sum = 0;
 
@@ -21,6 +26,8 @@ public class EulerianDistance {
 
 
 
+    // transform the the distance value into relevance value
+    // the smaller the distance is, the more relevance is the document
     private static double transformEulerianDistanceToRelevanceValue( double eulerianDistance ) {
 
         // 1/euler -> 1/10=0,1  1/1=1
@@ -32,6 +39,13 @@ public class EulerianDistance {
 
 
 
+    /**
+     * This method will calculate the relevance of a given document for a given search. The relevance calculation
+     * is using the euler distance.
+     * @param documentVector is the {@code Double} array representing the document vector
+     * @param searchVector is the {@code Double} array representing the search vector
+     * @return return the calculate relevance from document and search
+     */
     public static double calcRelevance( Double[] documentVector, Double[] searchVector ) {
 
         double distance = calEulerianDistance( documentVector, searchVector );
