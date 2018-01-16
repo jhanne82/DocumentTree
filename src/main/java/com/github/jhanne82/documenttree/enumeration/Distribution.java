ff2b1345304c205d0e60695d608f354b5066e96f
jhanne82 <jasmin.hannemann@googlemail.com>
2018-01-16 08:47:32 +0100

    - added documentation

--- src/main/java/com/github/jhanne82/documenttree/enumeration/Distribution.java
+++ src/main/java/com/github/jhanne82/documenttree/enumeration/Distribution.java
@@ -1,10 +1,26 @@
 package com.github.jhanne82.documenttree.enumeration;
 
 
-
+/**
+ *  Distributions that can be used to define the distribution of the document and search terms.
+ *  <li>{@link #EQUALLY}</li>
+ *  <li>{@link #GAUSSIAN}</li>
+ *  <li>{@link #EXPONENTIALLY}</li>
+ */
 public enum Distribution {
 
+    /**
+     *  Each possible value will occur with the same probability.
+     */
     EQUALLY,
+
+    /**
+     * The probability of the values are described by the Gaussian function.
+     */
     GAUSSIAN,
+
+    /**
+     * The probability of the possible values a given by the exponential function.
+     */
     EXPONENTIALLY
 }
